423e162e8d80c19b596618d1bbcc2e7aa412ce6f
Krittika-Saha <krittika.saha.dev@gmail.com>
2021-04-26 21:59:57 +0530

    Completed #TODO: 2. Authenticate with Spotify.

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
+SPOTIPY_CLIENT_ID="YOUR_SPOTIPY_CLIENT_ID"
+SPOTIPY_CLIENT_SECRET="YOUR_SPOTIFY_CLIENT_SECRET"
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=SPOTIPY_CLIENT_ID,
+        client_secret=SPOTIPY_CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 
 html_doc = requests.get(f"https://www.billboard.com/charts/hot-100/{date}").text
@@ -20,7 +36,9 @@ print(song_text)
 
 
 
+
+
 #TODO: 1. Scrape the Billboard Hot 100 for the songs of a particular date.✅
-#TODO: 2. Authenticate with Spotify
-#TODO: 3. Search Spotify with the scraped songs from Todo 1
+#TODO: 2. Authenticate with Spotify. ✅
+#TODO: 3. Search Spotify with the scraped songs from Todo 1.
 #TODO: 4. Create and add a spotify playlist.
