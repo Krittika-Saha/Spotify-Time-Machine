ee46722b1ed2bfab42ecfcea4a98dcd994725c42
StarTwinkies <krittika.saha.dev@gmail.com>
2021-04-27 14:01:59 +0530

    Completed TODO: 3. Search Spotify with the scraped songs from Todo 1.

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from pprint import pprint
 
-SPOTIPY_CLIENT_ID="YOUR_SPOTIPY_CLIENT_ID"
-SPOTIPY_CLIENT_SECRET="YOUR_SPOTIFY_CLIENT_SECRET"
+SPOTIPY_CLIENT_ID="cb6739d4db514e5388b215e0882bf249"
+SPOTIPY_CLIENT_SECRET="0bd3f310d6e6432d80d95831ff2518ed"
 
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
@@ -23,10 +24,18 @@ html_doc = requests.get(f"https://www.billboard.com/charts/hot-100/{date}").text
 soup = BeautifulSoup(html_doc, 'html.parser')
 
 songs = soup.find_all("span", class_="chart-element__information__song")
-song_text = [i.getText() for i in songs]
-print(song_text)
+song_names = [i.getText() for i in songs]
 
-
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    pprint(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
 
 
 
